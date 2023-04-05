3dd19cd3b7b5b3e9f02e90003670fd4db13f3880
laqudee <2268678583@qq.com>
2023-04-05 21:06:54 +0800

    feat 深入设计模式 中介模式 rust code

---
+++ design-patterns/mediator-pattern/src/trains/mod.rs
@@ -0,0 +1,14 @@
+mod freight_train;
+mod passenger_train;
+
+pub use freight_train::FreightTrain;
+pub use passenger_train::PassengerTrain;
+
+use crate::train_station::Mediator;
+
+// A train gets a mediator object by reference
+pub trait Train {
+    fn name(&self) -> &String;
+    fn arrive(&mut self, mediator: &mut dyn Mediator);
+    fn depart(&mut self, mediator: &mut dyn Mediator);
+}
